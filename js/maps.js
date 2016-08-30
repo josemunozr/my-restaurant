cc5eeb44b6a782744b53aa281e9f8a0be7cbd8e7
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-08-29 22:13:48 -0300

    - se agrega distancia con DistanceMatrixService desde geolocalizacion y localizacion del restaurant - se agrega marker de la posicion del restaurant

--- js/maps.js
+++ js/maps.js
@@ -18,20 +18,52 @@
 
   }
 
+  const my_place = {
+    lat: -33.468819,
+    lng: -70.57620199999997
+  }
+
   google.maps.event.addDomListener(window, "load", () => {
     const map = new google.maps.Map(
       document.getElementById('map'),
       {
-        center: {
-          lat: -33.468819,
-          lng: -70.57620199999997
-        },
+        center: my_place,
         zoom: 15
       }
     )
 
+    const marker = new google.maps.Marker({
+      map: map,
+      position: my_place,
+      title: 'My Restorant',
+      visible: true
+    })
+
     UserLocation.get((coords) => {
-      console.info(coords)
+      // calcular distancia del restaurante segun localizacion de usuario
+      let origen  = new google.maps.LatLng(coords.lat, coords.lng) // LatLng
+      let destino = new google.maps.LatLng(my_place.lat, my_place.lng)
+
+      let service = new google.maps.DistanceMatrixService()
+
+      service.getDistanceMatrix({
+        origins: [origen],
+        destinations: [destino],
+        travelMode: google.maps.TravelMode.DRIVING // manejando
+      }, (response, status) => {
+        if(status === google.maps.DistanceMatrixStatus.OK){
+          const duration_element = response.rows[0].elements[0]
+          const duracion_viaje = duration_element.duration.text
+          // response.rows[0] -> primer origen
+          // .rows[0].elements[0] -> primer destino
+
+          document.querySelector('#message')
+                  .innerHTML = `
+                    Estás a ${duracion_viaje} del probar la mejor comida en
+                    <span class="dancing-script medium">My Restorant</span>
+                  `
+        }
+      })
     })
 
   })
